0299627af8b2df5569aa089b4c02ecf2260ed7b7
Nick Freear <nfreear@yahoo.co.uk>
2014-01-20 11:05:05 +0000

    Added MSSQL UPDATE statement output - "sql_tbluser_update" function

--- kn-ldap.php
+++ kn-ldap.php
@@ -9,8 +9,11 @@
 
   Orphaned documents (ones with no current OU authors..)
 
+
+  C:\Users\ndf42\workspace\kn>\Users\ndf42\xampp\php\php kn-ldap.php  T:\KN-users-ou-doc-authors-2014-01-17.csv
+
   @copyright Copyright 2013 Nick Freear, The Open University.
-  @author    Nick Freear, 17 Dec 2013.
+  @author    Nick Freear, 17 December 2013.
 */
 require_once './config.php';
 require_once './lib/LdapOU.php';
@@ -21,6 +24,7 @@ $csv_out = './'. basename($csv_in, '.csv') .'-out.csv';
 $php_out = './'. basename($csv_in, '.csv') .'-php-serial.txt';
 $json_out = './'. basename($csv_in, '.csv') .'-json.txt';
 $email_out= './'. basename($csv_in, '.csv') .'-email.txt';
+$sql_out = './'. basename($csv_in, '.csv') .'-update.sql';
 
 
 //$is_library = (basename($argv[0]) !== basename(__FILE__));
@@ -34,8 +38,7 @@ if (! $csv_raw) {
 echo $csv_in;
 
 
-$obj = array();
-$email_list = '';
+$obj = $email_list = $user_ids = array();
 
 $fcsv = fopen($csv_out, 'w');
 
@@ -58,7 +61,9 @@ foreach ($csv_raw as $row) {
   $bytes = fputcsv($fcsv, (array)$row);
 
   if ($row->is_current) {
-    $email_list .= $row->email . EMAIL_SEP;
+    $email_list[] = $row->email;
+  } else {
+    $user_ids[] = $row->user_id;
   }
 
   if (LIMIT && count($obj) > LIMIT) {
@@ -67,11 +72,13 @@ foreach ($csv_raw as $row) {
 }
 
 fclose($fcsv);
-$bytes = file_put_contents($email_out, $email_list);
+$bytes = file_put_contents($email_out, implode(EMAIL_SEP, $email_list));
 #$bytes = file_put_contents($json_out, json_encode($obj));
-$bytes = file_put_contents($php_out, serialize($obj));
+$bytes = file_put_contents($sql_out, sql_tbluser_update($user_ids));
+$bytes = file_put_contents($php_out, serialize($obj));  //? serialize((array) $obj)
 
 echo "Files written, $bytes bytes: ". $php_out .PHP_EOL;
+echo "Current authors: ". count($email_list) .PHP_EOL;
 #var_dump($obj);
 
 
@@ -91,7 +98,7 @@ function csv_to_obj($row) {
 }
 
 /**
- * @link http://gist.github.com/385876
+ * @link https://gist.github.com/jaywilliams/385876
  */
 function csv_to_array($filename='', $delimiter=',')
 {
@@ -112,6 +119,39 @@ function csv_to_array($filename='', $delimiter=',')
         fclose($handle);
     }
     return $data;
+}
+
+/** Return MSSQL UPDATE statement.
+ * @link http://www.karlrixon.co.uk/writing/update-multiple-rows-with-different-values-and-a-single-sql-query/
+ */
+function sql_tbluser_update($user_ids) {
+  $user_ids = wordwrap(implode(', ', $user_ids), 80);
+  $mssql_template = <<<EOT
+--
+-- Auto-generated: {DATE}
+--
+
+USE [knowledge_network];
+
+ALTER TABLE [dbo].[tblUser] ADD isCurrent int NOT NULL DEFAULT(1);
+
+UPDATE tblUser SET isCurrent = 0 WHERE UserID = 1;
+
+
+--UPDATE [dbo].[tblUser]
+--    SET isCurrent = CASE UserID
+--        WHEN 1 THEN 0
+--        WHEN 2 THEN 1
+--        WHEN 3 THEN 0
+--    END
+--WHERE UserID IN (1,2,3)
+
+EOT;
+
+  return strtr($mssql_template, array(
+      'WHERE UserID = 1' => 'WHERE UserID in ('.PHP_EOL. $user_ids .PHP_EOL.')',
+      '{DATE}' => date('r')
+  ));
 }
 
 //End.
